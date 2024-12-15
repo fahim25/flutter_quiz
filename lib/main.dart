cbfbf2451f994c312319c43a13a50812b7582207
Fahim Muntasir <fahimmuntasir201@gmail.com>
2024-12-15 16:26:53 +0600

    refactor for changing screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quiz/gradiant_container.dart';
-import 'package:flutter_quiz/start_screen.dart';
-
-// void main() {
-//   runApp(
-//     const MaterialApp(
-//       home: Scaffold(
-//         // backgroundColor: Colors.amber,
-//         backgroundColor: Color.fromARGB(255, 47, 5, 120),
-//         body: GradientContainer(
-//           Color.fromARGB(255, 20, 4, 46),
-//           Color.fromARGB(255, 69, 29, 137),
-//           Color.fromARGB(255, 81, 67, 104)
-//         ),
-//       ),
-//     ),
-//   );
-// }
+import 'package:flutter_quiz/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 20, 4, 46),
-                Color.fromARGB(255, 69, 29, 137),
-                Color.fromARGB(255, 81, 67, 104)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 20, 4, 46),
+                Color.fromARGB(255, 69, 29, 137),
+                Color.fromARGB(255, 81, 67, 104)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
