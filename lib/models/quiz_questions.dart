74881c55ef8e0f4660518c7a19ac65c5165aef79
Fahim Muntasir <fahimmuntasir201@gmail.com>
2024-12-18 12:40:30 +0600

    moving around questions

--- lib/models/quiz_questions.dart
+++ lib/models/quiz_questions.dart
@@ -2,4 +2,10 @@ class QuizQuestions {
   QuizQuestions({required this.text, required this.answers});
   final String text;
   final List<String> answers;
+
+  List<String> getShuffledAns() {
+    final shuffledList = List.of(answers);
+    shuffledList.shuffle();
+    return shuffledList;
+  }
 }
--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,45 +12,60 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void ansQuestion() {
+    setState(() {
+      // currentQuestionIndex = currentQuestionIndex + 1;
+      // currentQuestionIndex += 1;
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          ...currentQuestion.answers.map((item) {
-            return AnsButton(btnText: item, onTap: () {});
-          })
+            const SizedBox(
+              height: 20,
+            ),
+            ...currentQuestion.getShuffledAns().map((item) {
+              return AnsButton(btnText: item, onTap: ansQuestion);
+            })
 
-          /* AnsButton(
-            btnText: currentQuestion.answers[0],
-            onTap: () {},
-          ),
-          AnsButton(
-            btnText: currentQuestion.answers[1],
-            onTap: () {},
-          ),
-          AnsButton(
-            btnText: currentQuestion.answers[2],
-            onTap: () {},
-          ),
-          AnsButton(
-            btnText: currentQuestion.answers[3],
-            onTap: () {},
-          ), */
-        ],
+            /* AnsButton(
+              btnText: currentQuestion.answers[0],
+              onTap: () {},
+            ),
+            AnsButton(
+              btnText: currentQuestion.answers[1],
+              onTap: () {},
+            ),
+            AnsButton(
+              btnText: currentQuestion.answers[2],
+              onTap: () {},
+            ),
+            AnsButton(
+              btnText: currentQuestion.answers[3],
+              onTap: () {},
+            ), */
+          ],
+        ),
       ),
     );
   }
